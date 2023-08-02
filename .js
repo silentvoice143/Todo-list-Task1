aaffa5d8758303327ff778c5c040ba0bd76e30e4
Satyam Kumar <silentvoice4143@gmail.com>
2023-08-02 17:55:41 +0530

    todov1-final

--- .js
+++
@@ -1,5 +0,0 @@
-var input = document.getElementById("check");
-var select = document.querySelectorAll(".select")[0];
-input.onchange = function () {
-  select.style.display = this.checked ? "block" : "none";
-};
